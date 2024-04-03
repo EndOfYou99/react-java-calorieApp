495d30b055faab47a365a2d34691ee9e688b92ee
kaan <k.yusein@proxiad.com>
2024-04-03 21:44:17 +0300

    [04] : added crossOrigin to apis, so they can be called from the front end

--- calorie-app/src/main/java/com/calorieapp/calorieapp/food/FoodController.java
+++ calorie-app/src/main/java/com/calorieapp/calorieapp/food/FoodController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,17 +20,20 @@ public class FoodController {
 	private FoodService foodService;
 
 	@GetMapping("/getAll")
+	@CrossOrigin(origins = "http://localhost:3000")
 	private ResponseEntity<List<Food>> getAll() {
 		return new ResponseEntity<List<Food>>(foodService.getAll(), HttpStatus.OK);
 	}
 
 	@PostMapping("/save")
+	@CrossOrigin(origins = "http://localhost:3000")
 	public ResponseEntity<Food> createFood(@RequestBody Food food) {
 		Food createdFood = foodService.saveFood(food);
 		return new ResponseEntity<>(createdFood, HttpStatus.CREATED);
 	}
 
 	@PostMapping("/totalNutrients")
+	@CrossOrigin(origins = "http://localhost:3000")
 	public TotalNutrientAmount getTotalNutrients(@RequestBody List<Food> foods) {
 		return foodService.calculateTotalNutrients(foods);
 	}
